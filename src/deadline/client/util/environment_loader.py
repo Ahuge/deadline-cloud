f5a865dd957a1e3ccdf40aad39ab049e1d1a8dcf
Alex Hughes <ahughesalex@gmail.com>
2024-10-23 16:07:56 -0700

    Abstract out the logic for loading DCC callbacks from the environment
    This logic can now easily be used by a DCC specific submitter to load any defined callbacks

---
+++ src/deadline/client/util/environment_loader.py
@@ -0,0 +1,54 @@
+import os
+
+
+from deadline.client.util import ui_callback, post_submit_callback, create_job_bundle_callback
+from deadline.client.exceptions import DeadlineOperationError
+
+
+def load_dcc_environment_callbacks(dcc_name: str):
+    on_create_job_bundle_callback = None
+    on_ui_callback = None
+    on_post_submit_callback = None
+
+    if os.path.exists(os.environ.get("DEADLINE_{dcc}_CREATE_JOB_BUNDLE_CALLBACK".format(dcc=dcc_name.upper()), "")):
+        try:
+            on_create_job_bundle_callback = create_job_bundle_callback.load_create_job_bundle_callback(
+                module_path=os.environ.get("DEADLINE_{dcc}_CREATE_JOB_BUNDLE_CALLBACK".format(dcc=dcc_name.upper()))
+            )
+        except Exception:
+            import traceback
+            raise DeadlineOperationError(
+                "Error while loading on_create_job_bundle_callback at {path}. {trace}".format(
+                    path=os.environ.get("DEADLINE_{dcc}_CREATE_JOB_BUNDLE_CALLBACK".format(dcc=dcc_name.upper())),
+                    trace=traceback.format_exc()
+                )
+            )
+
+    if os.path.exists(os.environ.get("DEADLINE_{dcc}_UI_CALLBACK".format(dcc=dcc_name.upper()), "")):
+        try:
+            on_ui_callback = ui_callback.load_ui_callback(
+                module_path=os.environ.get("DEADLINE_{dcc}_UI_CALLBACK".format(dcc=dcc_name.upper()))
+            )
+        except Exception:
+            import traceback
+            raise DeadlineOperationError(
+                "Error while loading on_pre_submit_callback at {path}. {trace}".format(
+                    path=os.environ.get("DEADLINE_{dcc}_UI_CALLBACK".format(dcc=dcc_name.upper())),
+                    trace=traceback.format_exc()
+                )
+            )
+
+    if os.path.exists(os.environ.get("DEADLINE_{dcc}_POST_SUBMIT_CALLBACK".format(dcc=dcc_name.upper()), "")):
+        try:
+            on_post_submit_callback = post_submit_callback.load_post_submit_callback(
+                module_path=os.environ.get("DEADLINE_{dcc}_POST_SUBMIT_CALLBACK".format(dcc=dcc_name.upper())),
+            )
+        except Exception:
+            import traceback
+            raise DeadlineOperationError(
+                "Error while loading on_post_submit_callback at {path}. {trace}".format(
+                    path=os.environ.get("DEADLINE_{dcc}_POST_SUBMIT_CALLBACK".format(dcc=dcc_name.upper())),
+                    trace=traceback.format_exc()
+                )
+            )
+    return on_ui_callback, on_post_submit_callback, on_create_job_bundle_callback
